6827d532f1774a3f3d65992b9cee1269a3eb63c9
Julie Hellec <hellecjulie@gmail.com>
2019-11-21 14:46:01 +0100

    Feat: mise en place component acceuil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './pages/acceuil/acceuil.component';
+import { LoginComponent } from './pages/acceuil/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
