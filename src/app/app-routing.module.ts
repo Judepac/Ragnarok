26abe1c1c34d18151a8b0e6f6ee1b4df6e82fcaf
Julie Hellec <hellecjulie@gmail.com>
2019-11-21 15:45:31 +0100

    Feat: installation angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AcceuilComponent } from './pages/acceuil/acceuil.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/acceuil' },
+  { path: '/acceuil', component: AcceuilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,28 +6,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './pages/acceuil/acceuil.component';
 import { LoginComponent } from './pages/acceuil/login/login.component';
-import { NavbarComponent } from './pages/main/navbar/navbar.component';
 import { WelcomeComponent } from './pages/main/welcome/welcome.component';
-import { SearchComponent } from './pages/main/search/search.component';
 import { MontresComponent } from './pages/montres/montres.component';
 import { MapComponent } from './pages/map/map.component';
 import { NotesComponent } from './pages/notes/notes.component';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AcceuilComponent,
     LoginComponent,
-    NavbarComponent,
     WelcomeComponent,
-    SearchComponent,
     MontresComponent,
     MapComponent,
     NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
